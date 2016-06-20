87cbe6c57a2474d35ab90741545ebb90457a81e6
Rob van Loon <borrob@me.com>
2016-06-21 00:10:55 +0200

    Cleanup code

--- src/com/webtimer/timer/AjaxTimerServlet.java
+++ src/com/webtimer/timer/AjaxTimerServlet.java
@@ -19,6 +19,7 @@ public class AjaxTimerServlet extends HttpServlet {
     	super();
     }
 
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("" + CountdownTimer.countdown/1000);
 	}
