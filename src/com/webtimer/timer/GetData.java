c19ca198d4473075e2b40d05652a8425541fbe90
Rob van Loon <borrob@me.com>
2016-08-30 22:34:59 +0200

    Added overall class to get 3 request in 1

---
+++ src/com/webtimer/timer/GetData.java
@@ -0,0 +1,33 @@
+package com.webtimer.timer;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.webtimer.timer.CountdownTimer;
+
+
+@WebServlet("/getData")
+public class GetData extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+    public GetData() {
+    	super();
+    }
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 * Returns a simpel text-string of the current countdown.
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append(String.valueOf(CountdownTimer.getCountdown()/1000));
+		response.getWriter().append("_").append(String.valueOf(CountdownTimer.getInterval()/1000));
+		response.getWriter().append("_").append(String.valueOf(CountdownTimer.getInterval2()/1000));
+	}
+
+}
