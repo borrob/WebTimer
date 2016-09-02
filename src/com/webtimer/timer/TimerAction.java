2f19152a591d83320ab4ec06ed0110b66532c3c3
Rob van Loon <borrob@me.com>
2016-09-02 19:42:31 +0200

    Explicitly import timer

--- src/com/webtimer/timer/TimerAction.java
+++ src/com/webtimer/timer/TimerAction.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.webtimer.timer.CountdownTimer;
+
 /**
  * Servlet implementation class TimerAction
  */
